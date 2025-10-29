2448b3179504a12dcdbad050611255e8022e9ebc
Aldi_Mustarih <aldimustarih46@gmail.com>
2025-10-29 17:49:44 +0700

    web tugas

--- blajar_web/dem.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("klik").addEventListener("clik",klik);
-
-
-function klik() {
-    document.getElementById("hasil").innerText = "tombol berhasil!"
-    alert("js berhasil");
-}
-
