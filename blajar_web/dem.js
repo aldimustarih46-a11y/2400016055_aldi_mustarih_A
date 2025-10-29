474f3a3c3592ce760ca7bb29eb7c95ed4c49945e
Aldi_Mustarih <aldimustarih46@gmail.com>
2025-10-29 12:15:19 +0700

    to do list

---
+++ blajar_web/dem.js
@@ -0,0 +1,8 @@
+document.getElementById("klik").addEventListener("clik",klik);
+
+
+function klik() {
+    document.getElementById("hasil").innerText = "tombol berhasil!"
+    alert("js berhasil");
+}
+
